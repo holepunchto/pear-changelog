0bbe2c4e791041e12ed270a6b205eba713eb2b42
butera-simone <111739570+butera-simone@users.noreply.github.com>
2023-06-14 16:31:41 +0200

    first (#2)
    * first
    
    * fixed tests and releasesAsObjects function
    
    * improved parsing
    
    * switchet to t.is()

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const b4a = require('b4a')
 function parse (b) {
   const stringedSource = typeof b === 'string' ? b : b4a.toString(b) // turn the buffer into a string
 
-  if (stringedSource.indexOf('\n## ') < 0) throw new Error('No releases or header found')
+  const firstReleaseIndex = stringedSource.indexOf('\n## ')
 
-  const noHeader = stringedSource.slice(stringedSource.indexOf('\n## ')) // removes the header of the changelog
+  if (firstReleaseIndex < 0) throw new Error('No releases or header found')
+
+  const noHeader = stringedSource.slice(firstReleaseIndex) // removes the header of the changelog
 
   const releasesAsStrings = noHeader.split(/(?=\n## )/) // splits changelog into an array of strings, one per release
 
-  const releasesAsObjects = releasesAsStrings.map(x => [x.trim().split('\n')[0].slice(x.indexOf('##') + 2).trim(), x]) // stores each string into an object with an id field
+  const releasesAsObjects = releasesAsStrings.map(parseRelease)
 
   return releasesAsObjects
 }
@@ -20,6 +22,13 @@ function diff (newLog, oldLog) {
   const oldMap = new Map(oldLog)
   const results = newLog.filter(x => !oldMap.has(x[0]))
   return results
+}
+
+function parseRelease (x) {
+  const clean = x.slice(x.indexOf('##')).trim()
+  const nl = clean.indexOf('\n')
+  const id = clean.slice(2, nl < 0 ? clean.length : nl).trim()
+  return [id, clean]
 }
 
 module.exports = { parse, diff }
